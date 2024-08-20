c1d5f01842592e25d32f54d7efd4641b0c83f725
Archana-AS <archanaas2321@gmail.com>
2024-08-20 19:03:47 +0530

    Add visitor and Navbar created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AddVisitor from './components/AddVisitor';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div >
+     <AddVisitor/>
+    </div>
+  );
+}
+
+export default App;
